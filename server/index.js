3568c5c04e93ca45bfc9b297a111dbb92c560e96
EYAD FAISAL <eyadevv@gmail.com>
2023-06-18 22:29:20 +0200

    Express server

---
+++ server/index.js
@@ -0,0 +1,7 @@
+const express = require("express")
+
+const app = express()
+app.get("/", (req, res) => {
+  res.send("Hello World!")
+})
+app.listen(3333, () => console.log("Server is Running on Port" + 3333))
