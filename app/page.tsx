af485bc63d44d4559b183fbee8acaa6d06d2c561
EYAD FAISAL <eyadevv@gmail.com>
2023-06-19 10:30:50 +0200

    added a Landing Page

--- app/page.tsx
+++ app/page.tsx
@@ -1,8 +1,26 @@
-const page = () => {
+import Head from "next/head"
+
+import { CallToAction } from "@/components/ui/CallToAction"
+import { Faqs } from "@/components/ui/Faqs"
+import { Footer } from "@/components/ui/Footer"
+import { Header } from "@/components/ui/Header"
+import { Hero } from "@/components/ui/Hero"
+import { Pricing } from "@/components/ui/Pricing"
+import { PrimaryFeatures } from "@/components/ui/PrimaryFeatures"
+import { SecondaryFeatures } from "@/components/ui/SecondaryFeatures"
+import { Testimonials } from "@/components/ui/Testimonials"
+
+export default function Home() {
   return (
-    <div>
-      <a href="/home"></a>
-    </div>
+    <main className="h-max w-screen bg-white dark:bg-black dark:text-white">
+      <Header />
+      <div>
+        <Hero />
+        <PrimaryFeatures />
+        <CallToAction />
+        <Testimonials />
+      </div>
+      <Footer />
+    </main>
   )
 }
-export default page
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,3 +1,5 @@
+const defaultTheme = require('tailwindcss/defaultTheme')
+
 /** @type {import('tailwindcss').Config} */
 module.exports = {
   content: [
@@ -6,24 +8,38 @@ module.exports = {
     "./app/**/*.{js,ts,jsx,tsx,mdx}",
   ],
   theme: {
-    extend: {
-      screens: {
-        sm: { max: "500px" },
-        md: { min: "500px", max: "715px" },
-        lg: { min: "715px" },
-      },
-      backgroundImage: {
-        "gradient-radial": "radial-gradient(var(--tw-gradient-stops))",
-        "gradient-conic":
-          "conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))",
-      },
-      backgroundSize: {
-        full: "100%",
-      },
-      colors: {
-        primary: "rgb(16 185 129)",
-      },
+    screens: {
+      sm: { max: "500px" },
+      md: { min: "500px", max: "715px" },
+      lg: { min: "715px" },
     },
-  },
-  plugins: [require("daisyui")],
+        fontSize: {
+      xs: ['0.75rem', { lineHeight: '1rem' }],
+      sm: ['0.875rem', { lineHeight: '1.5rem' }],
+      base: ['1rem', { lineHeight: '1.75rem' }],
+      lg: ['1.125rem', { lineHeight: '2rem' }],
+      xl: ['1.25rem', { lineHeight: '2rem' }],
+      '2xl': ['1.5rem', { lineHeight: '2rem' }],
+      '3xl': ['2rem', { lineHeight: '2.5rem' }],
+      '4xl': ['2.5rem', { lineHeight: '3.5rem' }],
+      '5xl': ['3rem', { lineHeight: '3.5rem' }],
+      '6xl': ['3.75rem', { lineHeight: '1' }],
+      '7xl': ['4.5rem', { lineHeight: '1.1' }],
+      '8xl': ['6rem', { lineHeight: '1' }],
+      '9xl': ['8rem', { lineHeight: '1' }],
+    },
+    extend: {
+      borderRadius: {
+        '4xl': '2rem',
+      },
+      fontFamily: {
+        sans: ['Inter', ...defaultTheme.fontFamily.sans],
+        display: ['Lexend', ...defaultTheme.fontFamily.sans],
+      },
+      maxWidth: {
+        '2xl': '40rem',
+      },
+    }},
+    plugins: [require('@tailwindcss/forms' ) , require('daisyui') ],
+
 }
