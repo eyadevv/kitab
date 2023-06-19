ac2ca5587fec3ed9fa3de8fcfbd85861d466444c
EYAD FAISAL <eyadevv@gmail.com>
2023-06-19 12:48:57 +0200

    Update route.ts

--- app/api/upload/route.ts
+++ app/api/upload/route.ts
@@ -3,38 +3,37 @@ import mime from "mime"
 import { join } from "path"
 import * as fs from 'node:fs/promises'
 import * as dateFn from 'date-fns'
+
+
+const Dir = async () => {
+  const relativeUploadDir = `/uploads/${dateFn.format(Date.now(), "dd-MM-Y")}`;
+  const path = join(process.cwd(), "public" , relativeUploadDir);
+  return path
+
+}
+const CheckOrCreateDir = async (path:string) => {
+  try {
+    await fs.stat(path)
+    return 'Dir is Already There'
+  } catch (error :any) {
+    return error.code === 'ENOENT'? await fs.mkdir(path , {recursive:true}) : 'Unknown Error'
+  }
+}
 export async function POST(request: Request) {
   const form = await request.formData()
-  const relativeUploadDir = `/uploads/${dateFn.format(Date.now(), "dd-MM-Y")}`;
-  const uploadDir = join(process.cwd(), "public" , relativeUploadDir);
-
-  const file = form.get('file') as Blob | null
+  const file = form.get('files') as Blob | null
+  const path = await Dir()
+  const output = await CheckOrCreateDir(path)
   if (!file) {
     return NextResponse.json(
       {error:"File Blob Not Found"},
       {status:404})
   }
   const buffers = Buffer.from(await file.arrayBuffer())
-  try {
-  await fs.stat(uploadDir)
-  } catch (error: any) {
-    if (error.code === 'ENOENT') {
-        await fs.mkdir(uploadDir , {recursive:true})
-    } else {
-      return NextResponse.json({
-        ERROR: "Some Thing Went Wrong",
-      } , {
-        status:500
-      })
-
-    }
-
-  }
-  // console.log(buffers.toString());
-  await fs.writeFile(uploadDir+'/test.pdf' ,buffers.toString()  ).then((res)=> console.log(res))
-  .then((err)=> console.log(err))
 
   return NextResponse.json({
-    name: "welcome to Upload Server",
-  })
+    msg: "File Uploaded",
+  },
+  {status:200}
+  )
 }
