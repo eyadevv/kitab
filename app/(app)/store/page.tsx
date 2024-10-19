f40b25314f4d35ef971eb7a98760cdfe2a7b6106
EYAD FAISAL <eyadevv@gmail.com>
2024-10-19 06:40:15 +0200

    remove unused code

--- app/(app)/store/page.tsx
+++
@@ -1,8 +0,0 @@
-const Store = () => {
-  return (
-    <div className="w-full h-full flex justify-center items-center" >
-      <h1 className="text-5xl font-extrabold" >SOON</h1>
-    </div>
-  )
-}
-export default Store
